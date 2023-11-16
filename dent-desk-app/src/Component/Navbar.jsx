135ee89f1f35a117efcb98d105a9c4bf5879eeef
Krishna Kumar <117253066+Krishu7827@users.noreply.github.com>
2023-11-17 02:21:18 +0530

    PopUp-Login-form-completed

--- dent-desk-app/src/Component/Navbar.jsx
+++ dent-desk-app/src/Component/Navbar.jsx
@@ -1,30 +1,60 @@
-
+//import Container from 'react-bootstrap/Container';
+//import Nav from 'react-bootstrap/Nav';
+//import Navbar from 'react-bootstrap/Navbar';
 import React, { useState } from 'react';
 import "./Navbar.module.css";
+import PopUpLogin from "./PopUpLogin"
 import img from "../ThynkCafe-fotor-20230824141827.png"
-import {useNavigate}   from "react-router-dom"
-import PopupLoginForm from './PopUpLogin';
+
+// function ReactNavbar() {
+//   return (
+//     <>
+//           <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary"  sticky="top" id='Navbar' bg='' data-bs-theme="" >
+//       <Container id='Navbar-Container'>
+//         <Navbar.Brand href="#home">
+//             <img  alt=""  className='Navbar-logo'/>
+
+//         </Navbar.Brand>
+//         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+//         <Navbar.Collapse id="responsive-navbar-nav">
+//           <Nav className="me-auto">
+//             <Nav.Link href="#features" id=''  >Home</Nav.Link>
+//             <Nav.Link href="#pricing" id='Navbar-Courses' >Courses</Nav.Link>
+//             <Nav.Link href="#pricing" id='Navbar-Cart'  ><img className='Navbar-Cart-img' src="" alt="" /></Nav.Link>
+//             <Nav.Link href="#pricing" id='Navbar-learning'  >My learning</Nav.Link>
+//           </Nav>
+//           <Nav>
+//             <Nav.Link href="#deets"style={{fontFamily:"'Oswald', sans-serif"}} >Signup</Nav.Link>
+//             <Nav.Link eventKey={2} href="#memes" id='Navbar-Login' style={{fontFamily:"'Oswald', sans-serif"}}>
+//               Login
+//             </Nav.Link>
+//           </Nav>
+//         </Navbar.Collapse>
+//       </Container>
+//     </Navbar>
+//     </>
+//   );
+// }
 
 
 
 
 function Navbar() {
- 
- let Navigation = useNavigate()
+  const [isOpen, setIsOpen] = useState(false);
+  const [isLogin,SetLogin] = useState(false)
+  console.log(isLogin)
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
- let NavigationHandler = ()=>{
-  Navigation("/")
- }
-
- let handler = ()=>{
-Navigation("/login")
- }
-
+  const toggleLogin = function(){
+    SetLogin(!isLogin)
+  }
 
   return (
     <nav>
     <div className="logo">
-    <img src={img} alt="" onClick={NavigationHandler}/>
+    <img src={img} alt="" />
     <h3>DentDesk</h3>
     </div>
     <input type="checkbox" id="click" />
@@ -32,22 +62,16 @@ Navigation("/login")
       <i className="fas fa-bars"></i>
     </label>
     <ul>
-      <li><a className="active" href="/booking">Service <span>|</span></a></li>
-      <li><a href="/appointment">My Appointment <span>|</span></a></li>
+      <li><a className="active" href="#">Service <span>|</span></a></li>
+      <li><a href="#">My Appointment <span>|</span></a></li>
       <li><a href="#">Past Appointment <span>|</span></a></li>
-     
-      {JSON.parse(localStorage.getItem("afterLogin"))?(<><li className='LoginName'><a>{JSON.parse(localStorage.getItem("afterLogin")).name}</a></li><li  style= {{cursor:"pointer"}}onClick={()=>{ localStorage.removeItem("afterLogin") 
-       Navigation("/")
-    }}><a>logout</a></li></>):(<><li><a href="#"onClick={handler} >Login<span>|</span></a></li>
-      <li><a href="#">Signup</a></li></>)}
-    
-
+      <li><a href="#" onClick={toggleLogin}>Login<span>|</span></a></li>
+      <li><a href="#">Signup</a></li>
     </ul>
-
+    {isLogin?<PopUpLogin/>:<PopUpLogin/>}
   </nav>
   );
 }
-
 
 
 
--- dent-desk-app/src/Component/PopUpLogin.jsx
+++ dent-desk-app/src/Component/PopUpLogin.jsx
@@ -1,104 +1,40 @@
-//import { GoogleLogin } from '@react-oauth/google';
-import React, {useState} from 'react';
-//import jwt_decoded from "jwt-decode"
-import { HomeRouter } from '../HomeRouter';
-
+import { GoogleLogin } from '@react-oauth/google';
+import React from 'react';
+import jwt_decoded from "jwt-decode"
 
 
 function PopupLoginForm() {
-
-    let [isOpen,SetisOpen]  = useState(true);
-let [email,Setemail] = useState("")
-
-let [password, SetPassword] = useState("")
-
-let loginHandler = async()=>{
-  console.log(email,password)
-    async function signup_fetch() {
-        try {
-            let responce = await fetch(`http://localhost:8080/login`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "Application/json",
-                },
-                body: JSON.stringify({email:email,password:password}),
-            });
-            if (responce.ok) {
-                let res = await responce.json();
-                alert(res.msg);
-     
-                localStorage.setItem("afterLogin",JSON.stringify(res.findmail[0]))
-            }
-        } catch (error) {
-            console.log("error", error);
-            alert("Something Went Wrong");
-        }
-    }
-
-    signup_fetch()
-}
-
-    if(!isOpen){
-        return <HomeRouter/>
-    }
-
-    // const loginSucess=(res)=>{
-    //     const decoded=jwt_decoded(res.credential);
-    //     console.log(decoded)
-    //     let obj={
-    //       "name":decoded.name,
-    //       "email":decoded.email,
-    //       "picture":decoded.picture,
-    //       "About":"Hey there i am using Whatsapp"
-    //     }
-    //     console.log(obj)
-    //     // dispatch(PostUsers(obj))
     
-    //     // setAccount(decoded)s
-    //   }
     
-    //   const LoginError=(res)=>{
        
-    //   }   
 
     return (
         <div className="container">
-            <div className="popup">
-                <div className="popup-content">
-                    <button className="close-popup-button" onClick={()=>{
-                        SetisOpen(!isOpen)
-                    }}>×</button>
-                    <div className="form-group">
-                        <h3>To Log in, enter email</h3>
-                        <input type="email" className="form-control" placeholder="Enter email" onChange={(event)=>{Setemail(event.target.value)}} />
-                    </div>
-                    <div className="form-group">
-                        <input type="password" className="form-control" placeholder="Password" onChange={(event)=>{SetPassword(event.target.value)}}/>
-                    </div>
-                    <div className="loginButtons">
-                        <div className="divOfloginButtons" onClick={loginHandler}>Continue</div>
-                        <a href="" className="forgot-pass">
-                            Forgot Password?
-                        </a>
-                    </div>
-                    <div className="LoginByGoogleOuath">
-                        <div className="LoginLine">
-                            <div className="gwMIGZ"></div>
-                            <h5>or</h5>
-                            <div className="gwMIGZ"></div>
-                        </div>
-                        <div className="GoogleOuathButton">
-                             <img src="https://cdn-icons-png.flaticon.com/128/300/300221.png" alt="" /> 
-                            {/* <GoogleLogin
-                                onSuccess={loginSucess}
-                                onError={LoginError}
-                            /> */}
-                            <p>Continue with Google</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <label for="show" className="close-btn fas fa-times" title="close"></label>
+        <div className="text">
+           Login Form
         </div>
+        <form action="#">
+           <div className="data">
+              <label>Email or Phone</label>
+              <input type="text" required/>
+           </div>
+           <div className="data">
+              <label>Password</label>
+              <input type="password" required/>
+           </div>
+           <div className="forgot-pass">
+              <a href="#">Forgot Password?</a>
+           </div>
+           <div className="btn">
+              <div className="inner"></div>
+              <button type="submit">login</button>
+           </div>
+           <div className="signup-link">
+              Not a member? <a href="#">Signup now</a>
+           </div>
+        </form>
+     </div>
     );
 }
 
